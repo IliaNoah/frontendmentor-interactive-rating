76569c6255fa992665ed6bb4781047d892e71612
IliaNoah <vaguaman86@gmail.com>
2022-11-27 19:19:16 +0330

    finished javascript part

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 "use strict";
 
-const submit = document.querySelector(".button");
-const message = document.querySelector(".message-page");
 const main = document.querySelector(".main");
+const submit = document.querySelector(".button");
+const messagePage = document.querySelector(".message-page");
+const message = document.querySelector(".message");
+
+let buttons = document.querySelectorAll(".buttons");
+
+for (let i = 0; i < buttons.length; i++) {
+  message.textContent = `Selected ${buttons[i].textContent} out of ${buttons[4].textContent}`;
+}
 
 submit.addEventListener("click", function () {
-  message.classList.remove("hidden");
+  messagePage.classList.remove("hidden");
   main.classList.add("hidden");
 });
